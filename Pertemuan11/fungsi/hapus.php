f2e36a01521f943170caae6d1cfc1c06fc0b4623
RatihParamita <significantratih@gmail.com>
2024-04-21 21:36:51 +0700

    Pertemuan11-Praktikum5

--- Pertemuan11/fungsi/hapus.php
+++ Pertemuan11/fungsi/hapus.php
@@ -12,6 +12,20 @@ if(!empty($_SESSION['username'])){
         } else{
             pesan('danger', "Jabatan tidak terhapus karena: " . mysqli_error($koneksi));
         } header("Location: ../index.php?page=jabatan");
+    } elseif (!empty($_GET['anggota'])) {
+        $id = antiinjection($koneksi, $_GET['id']);
+        $query = "DELETE FROM anggota WHERE user_id = '$id'";
+        if (mysqli_query($koneksi, $query)) {
+            $query2 = "DELETE FROM user WHERE id = '$id'";
+            if (mysqli_query($koneksi, $query2)) {
+                pesan('success', "Anggota telah terhapus.");
+            } else {
+                pesan('warning', "Data login terhapus tetapi data anggota tidak terhapus karena: " . mysqli_error($koneksi));
+            }
+        } else {
+            pesan('danger', "Anggota tidak terhapus karena: " . mysqli_error($koneksi));
+        }
+        header("Location: ../index.php?page=anggota");
     }
 }
 ?>
