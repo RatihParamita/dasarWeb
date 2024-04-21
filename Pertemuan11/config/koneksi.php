af94948363db3a9a70ebed4106e71bb77b006201
RatihParamita <significantratih@gmail.com>
2024-04-21 16:20:33 +0700

    Pertemuan11-Praktikum1

--- Pertemuan11/config/koneksi.php
+++ Pertemuan11/config/koneksi.php
@@ -1,5 +1,5 @@
 <?php
-date_default_timezone_get("Asia/Jakarta");
+date_default_timezone_set("Asia/Jakarta");
 $koneksi = mysqli_connect("localhost", "root", "", "prakwebdb");
 
 if(mysqli_connect_errno()){
